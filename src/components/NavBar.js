22cc5ad979963f4947e83903f86d69b17f7ad830
Isabel Pizarro <chabelipizarro1@gmail.com>
2021-01-07 23:02:05 +0100

    fix collase navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,27 +5,35 @@ import {
     Route,
     Link
   } from "react-router-dom";
+  import  { useState } from 'react';
   
   function NavBar () {
+    const [isOpen, setIsOpen] = useState('collapse navbar-collapse');
+      function show () {
+          console.log('click');
+          console.log(isOpen);
+          isOpen === 'open' ? setIsOpen('collapse navbar-collapse') : setIsOpen('open')
+      }
+      function showClose () {
+          setIsOpen('collapse navbar-collapse')
+      }
     return (
       <div >
           {/* <div className="bg-light"> <span>07845003045</span> <span>tombuzzcox@gmail.com</span></div> */}
         <nav class="navbar navbar-expand-lg navbar-light bg-light p-5">
               <div class="container-fluid">
                 <a class="navbar-brand" href="#">Tom Buzz Cox</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={show}>
                   <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class={isOpen} id="navbarNav" onClick={showClose}>
                   <ul class="navbar-nav">
                     <li class="">
-                      {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
                     <Link to="/">Home</Link>
   
                     </li>
                     <li class="nav-item">
                     <Link to="/about">About</Link>
-                      {/* <a class="nav-link" href="#">About</a> */}
                     </li>
                     <li class="nav-item">
                     <Link to="/contact">Contact</Link>
